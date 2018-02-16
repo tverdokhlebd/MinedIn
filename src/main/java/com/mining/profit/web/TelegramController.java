e76b5fda3c57b58112e3e2b600e64a1135c22db4
Dmitry Tverdokhleb <tverdokhlebd@gmail.com>
2018-02-16 23:03:04 +0200

    Added telegram controller and telegram library.

---
+++ src/main/java/com/mining/profit/web/TelegramController.java
@@ -0,0 +1,13 @@
+package com.mining.profit.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TelegramController {
+
+    @RequestMapping("/telegram")
+    public void telegram() {
+
+    }
+}
