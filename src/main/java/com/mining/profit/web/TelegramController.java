91ff9177640f17ab135e2af72ffdb28b0090df67
Dmitry Tverdokhleb <tverdokhlebd@gmail.com>
2018-02-24 00:48:12 +0200

    Refactored properties and added telegram web hook.

--- src/main/java/com/mining/profit/web/TelegramController.java
+++ src/main/java/com/mining/profit/web/TelegramController.java
@@ -1,7 +1,15 @@
 package com.mining.profit.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mining.profit.db.model.Configuration;
+import com.mining.profit.db.repository.ConfigurationRepository;
+import com.pengrad.telegrambot.BotUtils;
+import com.pengrad.telegrambot.model.Update;
 
 /**
  * REST controller for Telegram API.
@@ -13,9 +21,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/telegram")
 public class TelegramController {
 
-    @RequestMapping("/")
-    public void telegram() {
+    @Autowired
+    ConfigurationRepository configurationRepository;
 
+    @RequestMapping("/updates/{token}")
+    public void updates(@PathVariable("token") String token, @RequestBody String body) {
+        Configuration configuration = configurationRepository.findAll().get(0);
+        if (!configuration.getTelegramToken().equals(token)) {
+            return;
+        }
+        Update update = BotUtils.parseUpdate(body);
     }
 
 }
