7d96b8190afb27d766ae5993a65cc6f9b692ddd0
Dmitry Tverdokhleb <tverdokhlebd@gmail.com>
2018-04-06 22:22:27 +0300

    Added web response and calculating earnings.

--- src/main/java/com/mined/in/web/WebController.java
+++ src/main/java/com/mined/in/web/WebController.java
@@ -1,16 +1,28 @@
 package com.mined.in.web;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mined.in.coin.CoinType;
+import com.mined.in.earnings.Earnings;
+import com.mined.in.earnings.worker.EarningsWorker;
+import com.mined.in.earnings.worker.EarningsWorkerFactory;
+import com.mined.in.market.MarketRequestor;
+import com.mined.in.market.MarketRequestorFactory;
 import com.mined.in.market.MarketType;
+import com.mined.in.pool.AccountRequestor;
+import com.mined.in.pool.AccountRequestorFactory;
 import com.mined.in.pool.PoolType;
+import com.mined.in.reward.RewardRequestor;
+import com.mined.in.reward.RewardRequestorFactory;
 import com.mined.in.reward.RewardType;
 
 /**
@@ -29,8 +41,12 @@ public class WebController {
      * @return supporting coin types
      */
     @RequestMapping("/coins")
-    public List<CoinType> requestCoins() {
-        return Arrays.asList(CoinType.values());
+    public ResponseEntity<WebResponse<List<CoinType>>> requestCoins() {
+        try {
+            return new WebResponse<List<CoinType>>(Arrays.asList(CoinType.values())).create();
+        } catch (Exception e) {
+            return new WebResponse<List<CoinType>>().create(INTERNAL_SERVER_ERROR, e.getMessage());
+        }
     }
 
     /**
@@ -40,11 +56,16 @@ public class WebController {
      * @return supporting pool types by coin type
      */
     @RequestMapping("/pools")
-    public List<PoolType> requestPools(@RequestParam("coin") String coin) {
-        CoinType coinType = CoinType.valueOf(coin);
-        return Arrays.asList(PoolType.values()).stream().filter(pool -> {
-            return pool.getCoinTypeList().indexOf(coinType) != -1;
-        }).collect(Collectors.toList());
+    public ResponseEntity<WebResponse<List<PoolType>>> requestPools(@RequestParam("coin") String coin) {
+        try {
+            CoinType coinType = CoinType.valueOf(coin);
+            List<PoolType> poolTypeList = Arrays.asList(PoolType.values()).stream().filter(pool -> {
+                return pool.getCoinTypeList().indexOf(coinType) != -1;
+            }).collect(Collectors.toList());
+            return new WebResponse<List<PoolType>>(poolTypeList).create();
+        } catch (Exception e) {
+            return new WebResponse<List<PoolType>>().create(INTERNAL_SERVER_ERROR, e.getMessage());
+        }
     }
 
     /**
@@ -53,8 +74,12 @@ public class WebController {
      * @return supporting market types
      */
     @RequestMapping("/markets")
-    public List<MarketType> requestMarkets() {
-        return Arrays.asList(MarketType.values());
+    public ResponseEntity<WebResponse<List<MarketType>>> requestMarkets() {
+        try {
+            return new WebResponse<List<MarketType>>(Arrays.asList(MarketType.values())).create();
+        } catch (Exception e) {
+            return new WebResponse<List<MarketType>>().create(INTERNAL_SERVER_ERROR, e.getMessage());
+        }
     }
 
     /**
@@ -63,8 +88,39 @@ public class WebController {
      * @return supporting reward types
      */
     @RequestMapping("/rewards")
-    public List<RewardType> requestRewards() {
-        return Arrays.asList(RewardType.values());
+    public ResponseEntity<WebResponse<List<RewardType>>> requestRewards() {
+        try {
+            return new WebResponse<List<RewardType>>(Arrays.asList(RewardType.values())).create();
+        } catch (Exception e) {
+            return new WebResponse<List<RewardType>>().create(INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
+
+    /**
+     * Calculates earnings of pool account.
+     *
+     * @param coin coin type
+     * @param pool pool type
+     * @param market market type
+     * @param reward reward type
+     * @param walletAddress wallet address
+     * @return earnings of pool account
+     */
+    @RequestMapping("/calculate")
+    public ResponseEntity<WebResponse<Earnings>> requestCalculation(@RequestParam("coin") String coin, @RequestParam("pool") String pool,
+            @RequestParam("market") String market, @RequestParam("reward") String reward,
+            @RequestParam("wallet_address") String walletAddress) {
+        try {
+            CoinType coinType = CoinType.valueOf(coin);
+            AccountRequestor accountRequestor = AccountRequestorFactory.create(PoolType.valueOf(pool));
+            MarketRequestor marketRequestor = MarketRequestorFactory.create(MarketType.valueOf(market));
+            RewardRequestor rewardRequestor = RewardRequestorFactory.create(RewardType.valueOf(reward));
+            EarningsWorker worker = EarningsWorkerFactory.create(coinType, accountRequestor, marketRequestor, rewardRequestor);
+            Earnings earnings = worker.calculate(walletAddress);
+            return new WebResponse<Earnings>(earnings).create();
+        } catch (Exception e) {
+            return new WebResponse<Earnings>().create(INTERNAL_SERVER_ERROR, e.getMessage());
+        }
     }
 
 }
---
+++ src/main/java/com/mined/in/web/WebResponse.java
@@ -0,0 +1,90 @@
+package com.mined.in.web;
+
+import static org.springframework.http.HttpStatus.OK;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+/**
+ * Web response with custom format.
+ *
+ * @author Dmitry Tverdokhleb
+ *
+ * @param <T> type of data
+ */
+public class WebResponse<T> {
+
+    /** HTTP status. */
+    private HttpStatus status;
+    /** Response data. */
+    private T data;
+    /** Error message. */
+    private String error;
+
+    /**
+     * Creates the instance.
+     */
+    public WebResponse() {
+        super();
+    }
+
+    /**
+     * Creates the instance.
+     *
+     * @param data response data
+     */
+    public WebResponse(T data) {
+        this.data = data;
+    }
+
+    /**
+     * Creates response entity with successful status.
+     *
+     * @return response entity with successful status
+     */
+    public ResponseEntity<WebResponse<T>> create() {
+        this.status = HttpStatus.OK;
+        return new ResponseEntity<WebResponse<T>>(this, status);
+    }
+
+    /**
+     * Creates response entity with unsuccessful status.
+     *
+     * @param status HTTP status
+     * @param error error message
+     * @return response entity with unsuccessful status
+     */
+    public ResponseEntity<WebResponse<T>> create(HttpStatus status, String error) {
+        this.status = status;
+        this.error = error;
+        return new ResponseEntity<WebResponse<T>>(this, status);
+    }
+
+    /**
+     * Gets the data.
+     *
+     * @return the data
+     */
+    public T getData() {
+        return data;
+    }
+
+    /**
+     * Gets the error.
+     *
+     * @return the error
+     */
+    public String getError() {
+        return error;
+    }
+
+    /**
+     * Gets the status.
+     *
+     * @return the status
+     */
+    public boolean getStatus() {
+        return this.status == OK;
+    }
+
+}
