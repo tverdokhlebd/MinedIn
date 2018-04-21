908832ee0d048412aea1ceedff89509668fecca2
Dmitry Tverdokhleb <tverdokhlebd@gmail.com>
2018-04-21 13:32:40 +0300

    Optimization code for Market requestor.

---
+++ src/main/java/com/mined/in/market/coinmarketcap/BaseRequestor.java
@@ -0,0 +1,111 @@
+package com.mined.in.market.coinmarketcap;
+
+import static com.mined.in.http.ErrorCode.HTTP_ERROR;
+import static com.mined.in.http.ErrorCode.JSON_ERROR;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.mined.in.coin.CoinMarket;
+import com.mined.in.coin.CoinType;
+import com.mined.in.market.MarketRequestorException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+/**
+ * Base requestor of coin market.
+ *
+ * @author Dmitry Tverdokhleb
+ *
+ */
+public abstract class BaseRequestor {
+
+    /** HTTP client. */
+    private final OkHttpClient httpClient;
+    /** Endpoints update. */
+    private final int endpointsUpdate;
+    /** Next update of coin market. */
+    private static Date NEXT_UPDATE = new Date(0);
+    /** Cached coin market. */
+    private static CoinMarket COIN_MARKET;
+
+    /**
+     * Creates the instance.
+     *
+     * @param httpClient HTTP client
+     * @param endpointsUpdate endpoints update
+     */
+    public BaseRequestor(OkHttpClient httpClient, int endpointsUpdate) {
+        super();
+        this.httpClient = httpClient;
+        this.endpointsUpdate = endpointsUpdate;
+    }
+
+    /**
+     * Gets API url.
+     *
+     * @return API url
+     */
+    public abstract String getApiUrl();
+
+    /**
+     * Requests coin market.
+     *
+     * @return coin market
+     * @throws MarketRequestorException if there is any error in market requesting
+     */
+    public CoinMarket request() throws MarketRequestorException {
+        if (new Date().after(NEXT_UPDATE)) {
+            Request request = new Request.Builder().url(getApiUrl()).build();
+            try (Response response = httpClient.newCall(request).execute()) {
+                if (!response.isSuccessful()) {
+                    throw new MarketRequestorException(HTTP_ERROR, response.message());
+                }
+                try (ResponseBody body = response.body()) {
+                    JSONObject jsonResponse = new JSONArray(body.string()).getJSONObject(0);
+                    setNextUpdate(jsonResponse);
+                    createCoinMarket(jsonResponse);
+                }
+            } catch (JSONException e) {
+                throw new MarketRequestorException(JSON_ERROR, e);
+            } catch (IOException e) {
+                throw new MarketRequestorException(HTTP_ERROR, e);
+            }
+        }
+        return COIN_MARKET;
+    }
+
+    /**
+     * Sets next update.
+     *
+     * @param jsonResponse JSON response
+     */
+    private void setNextUpdate(JSONObject jsonResponse) {
+        Long lastUpdated = jsonResponse.getLong("last_updated");
+        Calendar now = Calendar.getInstance();
+        now.setTimeInMillis(lastUpdated * 1000);
+        now.add(Calendar.MINUTE, endpointsUpdate);
+        NEXT_UPDATE = now.getTime();
+    }
+
+    /**
+     * Creates coin market.
+     *
+     * @param jsonResponse JSON response
+     */
+    private void createCoinMarket(JSONObject jsonResponse) {
+        BigDecimal price = BigDecimal.valueOf(jsonResponse.getDouble("price_usd"));
+        CoinType coinType = CoinType.valueOf(jsonResponse.getString("symbol"));
+        COIN_MARKET = new CoinMarket(coinType, price);
+    }
+
+}
--- src/main/java/com/mined/in/market/coinmarketcap/EthereumClassicRequestor.java
+++ src/main/java/com/mined/in/market/coinmarketcap/EthereumClassicRequestor.java
@@ -1,25 +1,6 @@
 package com.mined.in.market.coinmarketcap;
 
-import static com.mined.in.http.ErrorCode.HTTP_ERROR;
-import static com.mined.in.http.ErrorCode.JSON_ERROR;
-
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.Calendar;
-import java.util.Date;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import com.mined.in.coin.CoinMarket;
-import com.mined.in.coin.CoinType;
-import com.mined.in.market.MarketRequestorException;
-
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import okhttp3.ResponseBody;
 
 /**
  * Requestor of ethereum classic coin market.
@@ -27,18 +8,10 @@ import okhttp3.ResponseBody;
  * @author Dmitry Tverdokhleb
  *
  */
-public class EthereumClassicRequestor {
+public class EthereumClassicRequestor extends BaseRequestor {
 
-    /** HTTP client. */
-    private final OkHttpClient httpClient;
-    /** Endpoints update. */
-    private final int endpointsUpdate;
     /** API url. */
     private static final String API_URL = "https://api.coinmarketcap.com/v1/ticker/ethereum-classic";
-    /** Next update of coin market. */
-    private static Date NEXT_UPDATE;
-    /** Cached coin market. */
-    private static CoinMarket COIN_MARKET;
 
     /**
      * Creates the instance.
@@ -47,61 +20,12 @@ public class EthereumClassicRequestor {
      * @param endpointsUpdate endpoints update
      */
     public EthereumClassicRequestor(OkHttpClient httpClient, int endpointsUpdate) {
-        super();
-        this.httpClient = httpClient;
-        this.endpointsUpdate = endpointsUpdate;
+        super(httpClient, endpointsUpdate);
     }
 
-    /**
-     * Requests ethereum classic coin market.
-     *
-     * @return ethereum classic coin market
-     * @throws MarketRequestorException if there is any error in market requesting
-     */
-    public CoinMarket request() throws MarketRequestorException {
-        Date currentDate = new Date();
-        if (NEXT_UPDATE == null || currentDate.after(NEXT_UPDATE)) {
-            Request request = new Request.Builder().url(API_URL).build();
-            try (Response response = httpClient.newCall(request).execute()) {
-                if (!response.isSuccessful()) {
-                    throw new MarketRequestorException(HTTP_ERROR, response.message());
-                }
-                try (ResponseBody body = response.body()) {
-                    JSONObject jsonResponse = new JSONArray(body.string()).getJSONObject(0);
-                    setNextUpdate(jsonResponse);
-                    createCoinMarket(jsonResponse);
-                }
-            } catch (JSONException e) {
-                throw new MarketRequestorException(JSON_ERROR, e);
-            } catch (IOException e) {
-                throw new MarketRequestorException(HTTP_ERROR, e);
-            }
-        }
-        return COIN_MARKET;
-    }
-
-    /**
-     * Sets next update.
-     *
-     * @param jsonResponse JSON response
-     */
-    private void setNextUpdate(JSONObject jsonResponse) {
-        Long lastUpdated = jsonResponse.getLong("last_updated");
-        Calendar now = Calendar.getInstance();
-        now.setTimeInMillis(lastUpdated * 1000);
-        now.add(Calendar.MINUTE, endpointsUpdate);
-        NEXT_UPDATE = now.getTime();
-    }
-
-    /**
-     * Creates coin market.
-     *
-     * @param jsonResponse JSON response
-     */
-    private void createCoinMarket(JSONObject jsonResponse) {
-        BigDecimal price = BigDecimal.valueOf(jsonResponse.getDouble("price_usd"));
-        CoinType coinType = CoinType.valueOf(jsonResponse.getString("symbol"));
-        COIN_MARKET = new CoinMarket(coinType, price);
+    @Override
+    public String getApiUrl() {
+        return API_URL;
     }
 
 }
